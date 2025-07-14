e9a39a2f1a78dde0fbd956712abc6bc0c678a97e
John Lindgren <john@jlindgren.net>
2025-07-13 23:24:27 -0400

    [pre-cpp] add g_seat global

--- src/xwayland.c
+++ src/xwayland.c
@@ -282,7 +282,7 @@ handle_request_move(struct wl_listener *listener, void *data)
 	 * want.
 	 */
 	struct view *view = wl_container_of(listener, view, request_move);
-	if (view == g_server.seat.pressed.view) {
+	if (view == g_seat.pressed.view) {
 		interactive_begin(view, LAB_INPUT_STATE_MOVE, LAB_EDGE_NONE);
 	}
 }
@@ -300,7 +300,7 @@ handle_request_resize(struct wl_listener *listener, void *data)
 	 */
 	struct wlr_xwayland_resize_event *event = data;
 	struct view *view = wl_container_of(listener, view, request_resize);
-	if (view == g_server.seat.pressed.view) {
+	if (view == g_seat.pressed.view) {
 		interactive_begin(view, LAB_INPUT_STATE_RESIZE, event->edges);
 	}
 }
@@ -623,7 +623,6 @@ handle_focus_in(struct wl_listener *listener, void *data)
 	struct xwayland_view *xwayland_view =
 		wl_container_of(listener, xwayland_view, focus_in);
 	struct view *view = &xwayland_view->base;
-	struct seat *seat = &g_server.seat;
 
 	if (!view->surface) {
 		/*
@@ -643,8 +642,8 @@ handle_focus_in(struct wl_listener *listener, void *data)
 		return;
 	}
 
-	if (view->surface != seat->seat->keyboard_state.focused_surface) {
-		seat_focus_surface(seat, view->surface);
+	if (view->surface != g_seat.seat->keyboard_state.focused_surface) {
+		seat_focus_surface(view->surface);
 	}
 }
 
@@ -808,7 +807,7 @@ xwayland_view_map(struct view *view)
 	 */
 	if (xwayland_view->focused_before_map) {
 		xwayland_view->focused_before_map = false;
-		seat_focus_surface(&g_server.seat, view->surface);
+		seat_focus_surface(view->surface);
 	}
 }
 
@@ -1108,7 +1107,7 @@ handle_server_ready(struct wl_listener *listener, void *data)
 static void
 handle_xwm_ready(struct wl_listener *listener, void *data)
 {
-	wlr_xwayland_set_seat(g_server.xwayland, g_server.seat.seat);
+	wlr_xwayland_set_seat(g_server.xwayland, g_seat.seat);
 	xwayland_update_workarea();
 }
 
@@ -1143,7 +1142,7 @@ xwayland_server_init(struct wlr_compositor *compositor)
 	}
 
 	struct wlr_xcursor *xcursor;
-	xcursor = wlr_xcursor_manager_get_xcursor(g_server.seat.xcursor_manager,
+	xcursor = wlr_xcursor_manager_get_xcursor(g_seat.xcursor_manager,
 		XCURSOR_DEFAULT, 1);
 	if (xcursor) {
 		struct wlr_xcursor_image *image = xcursor->images[0];
@@ -1179,7 +1178,7 @@ xwayland_reset_cursor(void)
 	}
 
 	struct wlr_xcursor *xcursor =
-		wlr_xcursor_manager_get_xcursor(g_server.seat.xcursor_manager,
+		wlr_xcursor_manager_get_xcursor(g_seat.xcursor_manager,
 			XCURSOR_DEFAULT, 1);
 
 	if (xcursor && !g_server.xwayland->xwm) {
