312f7ac12aa3fe88fd0a55f8ec46425fabaf795e
John Lindgren <john@jlindgren.net>
2025-07-20 16:04:21 -0400

    [cpp] use std::vector for rc.touch_configs

--- include/config/touch.h
+++ include/config/touch.h
@@ -2,17 +2,15 @@
 #ifndef LABWC_TOUCH_CONFIG_H
 #define LABWC_TOUCH_CONFIG_H
 
-#include <stdbool.h>
-#include <wayland-util.h>
+#include "common/str.h"
 
 struct touch_config_entry {
-	char *device_name;
-	char *output_name;
+	lab_str device_name;
+	lab_str output_name;
 	bool force_mouse_emulation;
-
-	struct wl_list link;     /* struct rcxml.touch_configs */
 };
 
-struct touch_config_entry *touch_find_config_for_device(char *device_name);
+struct touch_config_entry *touch_find_config_for_device(
+	const char *device_name);
 
 #endif /* LABWC_TOUCH_CONFIG_H */
