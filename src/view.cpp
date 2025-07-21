329074cde16b9a9d137a5013f2b96aefc329b3b6
John Lindgren <john@jlindgren.net>
2025-07-21 11:38:50 -0400

    [cpp] use reflist + weakptr for regions

--- src/view.cpp
+++ src/view.cpp
@@ -155,7 +155,7 @@ view_matches_query(struct view *view, struct view_query *query)
 	}
 
 	const char *tiled_region =
-		view->tiled_region ? view->tiled_region->name : NULL;
+		view->tiled_region ? view->tiled_region->name.c() : NULL;
 	if (!query_str_match(query->tiled_region.c(), tiled_region)) {
 		return false;
 	}
@@ -1086,11 +1086,11 @@ view_apply_region_geometry(struct view *view)
 	if (view->tiled_region_evacuate) {
 		/* View was evacuated from a destroying output */
 		/* Get new output local region, may be NULL */
-		view->tiled_region = regions_from_name(
-			view->tiled_region_evacuate, output);
+		view->tiled_region = weakptr(regions_from_name(
+			view->tiled_region_evacuate.c(), output));
 
 		/* Get rid of the evacuate instruction */
-		zfree(view->tiled_region_evacuate);
+		view->tiled_region_evacuate = lab_str();
 
 		if (!view->tiled_region) {
 			/* Existing region name doesn't exist in rc.xml anymore */
@@ -1326,8 +1326,8 @@ view_set_untiled(struct view *view)
 {
 	assert(view);
 	view->tiled = VIEW_EDGE_INVALID;
-	view->tiled_region = NULL;
-	zfree(view->tiled_region_evacuate);
+	view->tiled_region.reset();
+	view->tiled_region_evacuate = lab_str();
 	view_notify_tiled(view);
 }
 
@@ -1788,9 +1788,9 @@ view_evacuate_region(struct view *view)
 	assert(view);
 	assert(view->tiled_region);
 	if (!view->tiled_region_evacuate) {
-		view->tiled_region_evacuate = xstrdup(view->tiled_region->name);
+		view->tiled_region_evacuate = view->tiled_region->name;
 	}
-	view->tiled_region = NULL;
+	view->tiled_region.reset();
 }
 
 void
@@ -2106,7 +2106,7 @@ view_snap_to_region(struct view *view, struct region *region,
 		view_invalidate_last_layout_geometry(view);
 	}
 	view_set_untiled(view);
-	view->tiled_region = region;
+	view->tiled_region = weakptr(region);
 	view_notify_tiled(view);
 	view_apply_region_geometry(view);
 }
@@ -2131,8 +2131,10 @@ view_move_to_output(struct view *view, struct output *output)
 	} else if (view->tiled) {
 		view_apply_tiled_geometry(view);
 	} else if (view->tiled_region) {
-		struct region *region = regions_from_name(view->tiled_region->name, output);
-		view_snap_to_region(view, region, /*store_natural_geometry*/ false);
+		auto region =
+			regions_from_name(view->tiled_region->name.c(), output);
+		view_snap_to_region(view, region.get(),
+			/*store_natural_geometry*/ false);
 	}
 }
 
@@ -2400,10 +2402,6 @@ view::~view()
 
 	if (g_seat.pressed.view == view) {
 		seat_reset_pressed();
-	}
-
-	if (view->tiled_region_evacuate) {
-		zfree(view->tiled_region_evacuate);
 	}
 
 	if (view->inhibits_keybinds) {
