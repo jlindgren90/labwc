0d0274161cb5349105ab80ac94b26a4b032258a9
John Lindgren <john@jlindgren.net>
2025-07-21 11:34:30 -0400

    [cpp] use refptr/reflist for lab_data_buffer

--- include/view.h
+++ include/view.h
@@ -10,6 +10,7 @@
 #include "common/enum.h"
 #include "common/listener.h"
 #include "common/reflist.h"
+#include "common/str.h"
 #include "config.h"
 
 #define LAB_MIN_VIEW_HEIGHT 60
@@ -207,8 +208,8 @@ struct view_data {
 
 	/* used by scaled_icon_buffer */
 	struct {
-		char *name;
-		struct wl_array buffers; /* struct lab_data_buffer * */
+		lab_str name;
+		reflist<lab_data_buffer> buffers;
 	} icon;
 
 	struct {
@@ -576,7 +577,7 @@ void view_set_shade(struct view *view, bool shaded);
 
 /* Icon buffers set with this function are dropped later */
 void view_set_icon(struct view *view, const char *icon_name,
-	struct wl_array *buffers);
+	reflist<lab_data_buffer> &&buffers);
 
 struct view_size_hints view_get_size_hints(struct view *view);
 void view_adjust_size(struct view *view, int *w, int *h);
