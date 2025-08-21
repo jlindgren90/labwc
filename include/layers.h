58292e9e078ddc43d8b3666952e97d7da0508fc1
John Lindgren <john@jlindgren.net>
2025-08-21 00:43:59 -0400

    [cpp] use listeners for layers

--- include/layers.h
+++ include/layers.h
@@ -4,12 +4,13 @@
 
 #include <wayland-server-core.h>
 #include <wlr/util/box.h>
+#include "common/listener.h"
 
 struct server;
 struct output;
 struct seat;
 
-struct lab_layer_surface {
+struct lab_layer_surface : public destroyable {
 	struct wlr_layer_surface_v1 *layer_surface;
 	struct wlr_scene_layer_surface_v1 *scene_layer_surface;
 
@@ -17,25 +18,27 @@ struct lab_layer_surface {
 	/* true only inside handle_unmap() */
 	bool being_unmapped;
 
-	struct wl_listener map;
-	struct wl_listener unmap;
-	struct wl_listener surface_commit;
-	struct wl_listener output_destroy;
-	struct wl_listener node_destroy;
-	struct wl_listener new_popup;
+	~lab_layer_surface();
+
+	DECLARE_HANDLER(lab_layer_surface, map);
+	DECLARE_HANDLER(lab_layer_surface, unmap);
+	DECLARE_HANDLER(lab_layer_surface, commit);
+	DECLARE_HANDLER(lab_layer_surface, output_destroy);
+	DECLARE_HANDLER(lab_layer_surface, new_popup);
 };
 
-struct lab_layer_popup {
+struct lab_layer_popup : public destroyable {
 	struct wlr_xdg_popup *wlr_popup;
 	struct wlr_scene_tree *scene_tree;
 
 	/* To simplify moving popup nodes from the bottom to the top layer */
 	struct wlr_box output_toplevel_sx_box;
 
-	struct wl_listener commit;
-	struct wl_listener destroy;
-	struct wl_listener new_popup;
-	struct wl_listener reposition;
+	~lab_layer_popup();
+
+	DECLARE_HANDLER(lab_layer_popup, commit);
+	DECLARE_HANDLER(lab_layer_popup, new_popup);
+	DECLARE_HANDLER(lab_layer_popup, reposition);
 };
 
 void layers_init(void);
