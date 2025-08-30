ee08554ce24cf2a373600022618bbff9b59ad2de
John Lindgren <john@jlindgren.net>
2025-08-30 00:19:43 -0400

    [cpp] class-ify foreign_toplevel and friends

--- include/foreign-toplevel/foreign.h
+++ include/foreign-toplevel/foreign.h
@@ -2,12 +2,39 @@
 #ifndef LABWC_FOREIGN_TOPLEVEL_H
 #define LABWC_FOREIGN_TOPLEVEL_H
 
-struct view;
-struct foreign_toplevel;
+#include <assert.h>
+#include "foreign-toplevel/ext-foreign.h"
+#include "foreign-toplevel/wlr-foreign.h"
 
-struct foreign_toplevel *foreign_toplevel_create(struct view *view);
-void foreign_toplevel_set_parent(struct foreign_toplevel *toplevel,
-	struct foreign_toplevel *parent);
-void foreign_toplevel_destroy(struct foreign_toplevel *toplevel);
+struct view;
+
+class foreign_toplevel
+{
+private:
+	weakptr<wlr_foreign_toplevel> wlr_toplevel;
+	weakptr<ext_foreign_toplevel> ext_toplevel;
+
+public:
+	foreign_toplevel(struct view *view) :
+		wlr_toplevel(wlr_foreign_toplevel::create(view)),
+		ext_toplevel(ext_foreign_toplevel::create(view)) {}
+
+	~foreign_toplevel() {
+		if (CHECK_PTR(wlr_toplevel, wt)) {
+			wt->destroy();
+			assert(!wlr_toplevel);
+		}
+		if (CHECK_PTR(ext_toplevel, et)) {
+			et->destroy();
+			assert(!ext_toplevel);
+		}
+	}
+
+	void set_parent(foreign_toplevel &parent) {
+		if (CHECK_PTR(wlr_toplevel, wt)) {
+			wt->set_parent(parent.wlr_toplevel.get());
+		}
+	}
+};
 
 #endif /* LABWC_FOREIGN_TOPLEVEL_H */
