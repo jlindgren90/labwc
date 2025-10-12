61ba44940497eca6431bc2225787d4b03ffa3ff0
John Lindgren <john@jlindgren.net>
2025-10-12 14:37:51 -0400

    [cpp] make lab_img value-based

--- include/img/img.h
+++ include/img/img.h
@@ -2,10 +2,13 @@
 #ifndef LABWC_IMG_H
 #define LABWC_IMG_H
 
-#include <cairo.h>
-#include <stdbool.h>
-#include <wayland-util.h>
+#include <vector>
 #include "buffer.h"
+#include "config.h"
+
+#if HAVE_RSVG
+typedef struct _RsvgHandle RsvgHandle;
+#endif
 
 enum lab_img_type {
 	LAB_IMG_PNG,
@@ -14,65 +17,57 @@ enum lab_img_type {
 	LAB_IMG_XPM,
 };
 
-struct lab_img {
-	struct wl_array modifiers; /* lab_img_modifier_func_t */
-	struct lab_img_data *data;
+struct lab_img_data : public ref_owned<lab_img_data> {
+	refptr<lab_data_buffer> buffer; // for PNG/XBM/XPM image
+#if HAVE_RSVG
+	RsvgHandle *svg; // for SVG image
+#endif
+
+	~lab_img_data();
 };
-
-struct lab_img *lab_img_load(enum lab_img_type type, const char *path,
-	float *xbm_color);
-
-/**
- * lab_img_load_from_bitmap() - create button from monochrome bitmap
- * @bitmap: bitmap data array in hexadecimal xbm format
- * @rgba: color
- *
- * Example bitmap: char button[6] = { 0x3f, 0x3f, 0x21, 0x21, 0x21, 0x3f };
- */
-struct lab_img *lab_img_load_from_bitmap(const char *bitmap, float *rgba);
 
 typedef void (*lab_img_modifier_func_t)(cairo_t *cairo, int w, int h);
 
-/**
- * lab_img_copy() - Copy lab_img
- * @img: source image
- *
- * This function duplicates lab_img, but its internal cache for the image is
- * shared.
- */
-struct lab_img *lab_img_copy(struct lab_img *img);
+struct lab_img {
+	// Shared internal image cache
+	refptr<lab_img_data> data;
 
-/**
- * lab_img_add_modifier() - Add a modifier function to lab_img
- * @img: source image
- * @modifier: function that applies modifications to the image.
- * @theme: pointer to theme passed to @modifier.
- *
- * "Modifiers" are functions that perform some additional drawing operation
- * after the image is rendered on a buffer with lab_img_render(). For example,
- * hover effects for window buttons can be drawn over the rendered image.
- */
-void lab_img_add_modifier(struct lab_img *img, lab_img_modifier_func_t modifier);
+	// "Modifiers" are functions that perform some additional
+	// drawing operation after the image is rendered on a buffer
+	// with lab_img_render(). For example, hover effects for window
+	// buttons can be drawn over the rendered image.
+	std::vector<lab_img_modifier_func_t> modifiers;
 
-/**
- * lab_img_render() - Render lab_img to a buffer
- * @img: source image
- * @width: width of the created buffer
- * @height: height of the created buffer
- * @scale: scale of the created buffer
- */
-refptr<lab_data_buffer> lab_img_render(struct lab_img *img, int width,
-	int height, double scale);
+	lab_img() {}
 
-/**
- * lab_img_destroy() - destroy lab_img
- * @img: lab_img to destroy
- */
-void lab_img_destroy(struct lab_img *img);
+	bool valid() const { return (bool)data; }
 
-/**
- * lab_img_equal() - Returns true if two images draw the same content
- */
-bool lab_img_equal(struct lab_img *img_a, struct lab_img *img_b);
+	bool operator==(const lab_img &other) const {
+		return data == other.data && modifiers == other.modifiers;
+	}
+	bool operator!=(const lab_img &other) const {
+		return data != other.data || modifiers != other.modifiers;
+	}
+
+	refptr<lab_data_buffer> render(int width, int height, double scale);
+
+	static lab_img load(lab_img_type type, const char *path,
+		float *xbm_color);
+
+	// Creates button from monochrome bitmap
+	//   @bitmap: bitmap data array in hexadecimal xbm format
+	//   @rgba: color
+	//
+	// Example bitmap: { 0x3f, 0x3f, 0x21, 0x21, 0x21, 0x3f }
+	static lab_img load_from_bitmap(const char *bitmap, float *rgba);
+
+private:
+	explicit lab_img(refptr<lab_data_buffer> buffer)
+		: data(buffer ? new lab_img_data{.buffer = buffer} : nullptr) {}
+#if HAVE_RSVG
+	explicit lab_img(RsvgHandle *svg)
+		: data(svg ? new lab_img_data{.svg = svg} : nullptr) {}
+#endif
+};
 
 #endif /* LABWC_IMG_H */
--- src/img/img.cpp
+++ src/img/img.cpp
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 #include "img/img.h"
-#include <assert.h>
-#include "buffer.h"
-#include "config.h"
 #include "common/box.h"
 #include "common/graphic-helpers.h"
 #include "common/macros.h"
@@ -17,20 +14,6 @@
 #include "labwc.h"
 #include "theme.h"
 
-struct lab_img_data {
-	enum lab_img_type type;
-	/* lab_img_data is refcounted to be shared by multiple lab_imgs */
-	int refcount;
-
-	/* Handler for the loaded image file */
-	refptr<lab_data_buffer> buffer; /* for PNG/XBM/XPM image */
-#if HAVE_RSVG
-	RsvgHandle *svg; /* for SVG image */
-#endif
-
-	~lab_img_data();
-};
-
 lab_img_data::~lab_img_data()
 {
 #if HAVE_RSVG
@@ -40,107 +23,58 @@ lab_img_data::~lab_img_data()
 #endif
 }
 
-static struct lab_img *
-create_img(struct lab_img_data *img_data)
-{
-	auto img = new lab_img();
-	img->data = img_data;
-	img_data->refcount++;
-	wl_array_init(&img->modifiers);
-	return img;
-}
-
-struct lab_img *
-lab_img_load(enum lab_img_type type, const char *path, float *xbm_color)
+lab_img
+lab_img::load(enum lab_img_type type, const char *path, float *xbm_color)
 {
 	if (string_null_or_empty(path)) {
-		return NULL;
+		return lab_img();
 	}
-
-	auto img_data = new lab_img_data();
-	img_data->type = type;
 
 	switch (type) {
 	case LAB_IMG_PNG:
-		img_data->buffer = img_png_load(path);
-		break;
+		return lab_img(img_png_load(path));
 	case LAB_IMG_XBM:
 		assert(xbm_color);
-		img_data->buffer = img_xbm_load(path, xbm_color);
-		break;
+		return lab_img(img_xbm_load(path, xbm_color));
 	case LAB_IMG_XPM:
-		img_data->buffer = img_xpm_load(path);
-		break;
+		return lab_img(img_xpm_load(path));
 	case LAB_IMG_SVG:
 #if HAVE_RSVG
-		img_data->svg = img_svg_load(path);
+		return lab_img(img_svg_load(path));
 #endif
 		break;
 	}
 
-	bool img_is_loaded = (bool)img_data->buffer;
-#if HAVE_RSVG
-	img_is_loaded |= (bool)img_data->svg;
-#endif
-
-	if (img_is_loaded) {
-		return create_img(img_data);
-	} else {
-		delete img_data;
-		return NULL;
-	}
+	return lab_img();
 }
 
-struct lab_img *
-lab_img_load_from_bitmap(const char *bitmap, float *rgba)
+lab_img
+lab_img::load_from_bitmap(const char *bitmap, float *rgba)
 {
-	auto buffer = img_xbm_load_from_bitmap(bitmap, rgba);
-	if (!buffer) {
-		return NULL;
-	}
-
-	auto img_data = new lab_img_data();
-	img_data->type = LAB_IMG_XBM;
-	img_data->buffer = buffer;
-
-	return create_img(img_data);
-}
-
-struct lab_img *
-lab_img_copy(struct lab_img *img)
-{
-	struct lab_img *new_img = create_img(img->data);
-	wl_array_copy(&new_img->modifiers, &img->modifiers);
-	return new_img;
-}
-
-void
-lab_img_add_modifier(struct lab_img *img,  lab_img_modifier_func_t modifier)
-{
-	lab_img_modifier_func_t *mod = wl_array_add(&img->modifiers, sizeof(*mod));
-	*mod = modifier;
+	return lab_img(img_xbm_load_from_bitmap(bitmap, rgba));
 }
 
 refptr<lab_data_buffer>
-lab_img_render(struct lab_img *img, int width, int height, double scale)
+lab_img::render(int width, int height, double scale)
 {
 	refptr<lab_data_buffer> buffer;
 
+	CHECK_PTR_OR_RET_VAL(this->data, data, buffer);
+
 	/* Render the image into the buffer for the given size */
-	if (CHECK_PTR(img->data->buffer, orig)) {
+	if (CHECK_PTR(data->buffer, orig)) {
 		buffer = buffer_scale_cairo_surface(orig->surface, width,
 			height, scale);
 #if HAVE_RSVG
-	} else if (img->data->svg) {
-		buffer = img_svg_render(img->data->svg, width, height, scale);
+	} else if (data->svg) {
+		buffer = img_svg_render(data->svg, width, height, scale);
 #endif
 	}
 
 	if (CHECK_PTR(buffer, buf)) {
 		/* Apply modifiers to the buffer (e.g. draw hover overlay) */
 		cairo_t *cairo = cairo_create(buf->surface);
-		lab_img_modifier_func_t *modifier;
-		wl_array_for_each(modifier, &img->modifiers) {
+		for (auto modifier : modifiers) {
 			cairo_save(cairo);
 			(*modifier)(cairo, width, height);
 			cairo_restore(cairo);
@@ -151,35 +85,4 @@ lab_img_render(struct lab_img *img, int width, int height, double scale)
 	}
 
 	return buffer;
-}
-
-void
-lab_img_destroy(struct lab_img *img)
-{
-	if (!img) {
-		return;
-	}
-
-	img->data->refcount--;
-	if (img->data->refcount == 0) {
-		delete img->data;
-	}
-
-	wl_array_release(&img->modifiers);
-	delete img;
-}
-
-bool
-lab_img_equal(struct lab_img *img_a, struct lab_img *img_b)
-{
-	if (img_a == img_b) {
-		return true;
-	}
-	if (!img_a || !img_b || img_a->data != img_b->data
-			|| img_a->modifiers.size != img_b->modifiers.size) {
-		return false;
-	}
-	return img_a->modifiers.size == 0
-		|| !memcmp(img_a->modifiers.data, img_b->modifiers.data,
-			img_a->modifiers.size);
 }
