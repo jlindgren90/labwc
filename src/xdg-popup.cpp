3c93ba9574b13e16d54369ca9f3bbfbdd432e6c6
John Lindgren <john@jlindgren.net>
2025-10-16 19:30:35 -0400

    [cpp] use std::variant for node_descriptor::data

--- src/xdg-popup.cpp
+++ src/xdg-popup.cpp
@@ -144,5 +144,5 @@ xdg_popup_create(struct view *view, struct wlr_xdg_popup *wlr_popup)
 	wlr_popup->base->surface->data =
 		wlr_scene_xdg_surface_create(parent_tree, wlr_popup->base);
 	node_descriptor_create((wlr_scene_node *)wlr_popup->base->surface->data,
-		LAB_NODE_XDG_POPUP, view, /*data*/ NULL);
+		LAB_NODE_XDG_POPUP, view);
 }
