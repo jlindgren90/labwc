f04572bc214227fd5cb76d092291edcdc4f9fc38
John Lindgren <john@jlindgren.net>
2025-11-23 23:26:43 -0500

    [cpp] class-ify foreign_toplevel and friends

--- include/foreign-toplevel/ext-foreign.h
+++ include/foreign-toplevel/ext-foreign.h
@@ -2,26 +2,31 @@
 #ifndef LABWC_EXT_FOREIGN_TOPLEVEL_H
 #define LABWC_EXT_FOREIGN_TOPLEVEL_H
 
-#include <wayland-server-core.h>
+#include "common/listener.h"
+#include "common/refptr.h"
 
-struct ext_foreign_toplevel {
-	struct view *view;
-	struct wlr_ext_foreign_toplevel_handle_v1 *handle;
+struct view;
+struct wlr_ext_foreign_toplevel_handle_v1;
 
-	/* Client side events */
-	struct {
-		struct wl_listener handle_destroy;
-	} on;
+class ext_foreign_toplevel : public destroyable,
+		public weak_target<ext_foreign_toplevel>
+{
+private:
+	view *const m_view;
+	wlr_ext_foreign_toplevel_handle_v1 *const m_handle;
+
+	// private constructor, use create() instead
+	ext_foreign_toplevel(view *view,
+		wlr_ext_foreign_toplevel_handle_v1 *handle);
 
 	/* Compositor side state updates */
-	struct {
-		struct wl_listener new_app_id;
-		struct wl_listener new_title;
-	} on_view;
-};
+	DECLARE_HANDLER(ext_foreign_toplevel, new_app_id);
+	DECLARE_HANDLER(ext_foreign_toplevel, new_title);
 
-void ext_foreign_toplevel_init(struct ext_foreign_toplevel *ext_toplevel,
-	struct view *view);
-void ext_foreign_toplevel_finish(struct ext_foreign_toplevel *ext_toplevel);
+public:
+	static ext_foreign_toplevel *create(view *view);
+
+	void destroy();
+};
 
 #endif /* LABWC_EXT_FOREIGN_TOPLEVEL_H */
