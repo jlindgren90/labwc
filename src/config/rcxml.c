dddaf6c829ab8ec40908a9d38e97335da2a6c037
John Lindgren <john@jlindgren.net>
2026-01-08 11:16:45 -0500

    [scope] disable adaptive sync for now

--- src/config/rcxml.c
+++ src/config/rcxml.c
@@ -650,21 +650,6 @@ fill_font(xmlNode *node)
 	}
 }
 
-static void
-set_adaptive_sync_mode(const char *str, enum adaptive_sync_mode *variable)
-{
-	if (!strcasecmp(str, "fullscreen")) {
-		*variable = LAB_ADAPTIVE_SYNC_FULLSCREEN;
-	} else {
-		int ret = parse_bool(str, -1);
-		if (ret == 1) {
-			*variable = LAB_ADAPTIVE_SYNC_ENABLED;
-		} else {
-			*variable = LAB_ADAPTIVE_SYNC_DISABLED;
-		}
-	}
-}
-
 /* Returns true if the node's children should also be traversed */
 static bool
 entry(xmlNode *node, char *nodename, char *content)
@@ -711,8 +696,6 @@ entry(xmlNode *node, char *nodename, char *content)
 		}
 	} else if (!strcmp(nodename, "gap.core")) {
 		rc.gap = atoi(content);
-	} else if (!strcasecmp(nodename, "adaptiveSync.core")) {
-		set_adaptive_sync_mode(content, &rc.adaptive_sync);
 	} else if (!strcasecmp(nodename, "autoEnableOutputs.core")) {
 		set_bool(content, &rc.auto_enable_outputs);
 	} else if (!strcasecmp(nodename, "reuseOutputMode.core")) {
@@ -873,7 +856,6 @@ rcxml_init(void)
 	rc.corner_radius = 8;
 
 	rc.gap = 0;
-	rc.adaptive_sync = LAB_ADAPTIVE_SYNC_DISABLED;
 	rc.auto_enable_outputs = true;
 	rc.reuse_output_mode = false;
 	rc.xwayland_persistence = false;
