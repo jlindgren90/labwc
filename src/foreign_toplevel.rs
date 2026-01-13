ae9c4cc2ea3ac77ac0055724c69da23c7904ca9e
John Lindgren <john@jlindgren.net>
2026-01-13 12:50:57 -0500

    migrate foreign_toplevel support (partial)

---
+++ src/foreign_toplevel.rs
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-2.0-only
+//
+use crate::bindings::*;
+use std::ffi::CString;
+
+pub struct ForeignToplevel {
+    pub res: *mut WlResource,
+}
+
+impl ForeignToplevel {
+    pub fn new(client: *mut WlResource, view_ptr: *mut CView) -> Self {
+        Self {
+            res: unsafe { foreign_toplevel_create(client, view_ptr) },
+        }
+    }
+
+    pub fn send_app_id(&self, app_id: &CString) {
+        unsafe { foreign_toplevel_send_app_id(self.res, app_id.as_ptr()) };
+    }
+
+    pub fn send_title(&self, title: &CString) {
+        unsafe { foreign_toplevel_send_title(self.res, title.as_ptr()) };
+    }
+
+    pub fn send_state(&self, state: ForeignToplevelState) {
+        unsafe { foreign_toplevel_send_state(self.res, state) };
+    }
+
+    pub fn send_done(&self) {
+        unsafe { foreign_toplevel_send_done(self.res) };
+    }
+
+    pub fn close(&self) {
+        unsafe { foreign_toplevel_close(self.res) };
+    }
+}
