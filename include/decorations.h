929a885450f450c3ec3f353bf3c04ed556b4d811
John Lindgren <john@jlindgren.net>
2026-02-19 15:15:21 -0500

    rework xdg-decoration-unstable-v1 impl

--- include/decorations.h
+++ include/decorations.h
@@ -2,12 +2,13 @@
 #ifndef LABWC_DECORATIONS_H
 #define LABWC_DECORATIONS_H
 
+struct wl_display;
+
 void kde_server_decoration_init(void);
-void xdg_server_decoration_init(void);
-
 void kde_server_decoration_update_default(void);
-
 void kde_server_decoration_finish(void);
-void xdg_server_decoration_finish(void);
+
+void xdg_deco_manager_init(struct wl_display *display);
+void xdg_deco_manager_finish(void);
 
 #endif /* LABWC_DECORATIONS_H */
