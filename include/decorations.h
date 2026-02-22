178731c44fcf9d78d660a36fb5952b945148f515
John Lindgren <john@jlindgren.net>
2026-02-22 11:11:14 -0500

    [pre] remove some xdg-shell workarounds

--- include/decorations.h
+++ include/decorations.h
@@ -2,15 +2,10 @@
 #ifndef LABWC_DECORATIONS_H
 #define LABWC_DECORATIONS_H
 
-struct server;
-struct view;
-struct wlr_surface;
-
 void kde_server_decoration_init(void);
 void xdg_server_decoration_init(void);
 
 void kde_server_decoration_update_default(void);
-void kde_server_decoration_set_view(struct view *view, struct wlr_surface *surface);
 
 void kde_server_decoration_finish(void);
 void xdg_server_decoration_finish(void);
