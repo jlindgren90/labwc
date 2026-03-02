652100b214841effa0115e703cca4a448790575f
John Lindgren <john@jlindgren.net>
2026-03-02 18:14:39 -0500

    simplify handling of unmanaged surfaces

--- include/xwayland.h
+++ include/xwayland.h
@@ -2,39 +2,7 @@
 #ifndef LABWC_XWAYLAND_H
 #define LABWC_XWAYLAND_H
 
-#include "view.h"
-
 struct wlr_compositor;
-struct wlr_output;
-struct wlr_output_layout;
-
-struct xwayland_unmanaged {
-	struct wlr_xwayland_surface *xwayland_surface;
-	struct wlr_scene_node *node;
-
-	struct wl_listener associate;
-	struct wl_listener dissociate;
-	struct wl_listener map;
-	struct wl_listener unmap;
-	struct wl_listener grab_focus;
-	struct wl_listener request_activate;
-	struct wl_listener request_configure;
-/*	struct wl_listener request_fullscreen; */
-	struct wl_listener set_geometry;
-	struct wl_listener destroy;
-	struct wl_listener set_override_redirect;
-
-	/*
-	 * True if the surface has performed a keyboard grab. labwc
-	 * honors keyboard grabs and will give the surface focus when
-	 * it's mapped (which may occur slightly later) and on top.
-	 */
-	bool ever_grabbed_focus;
-};
-
-void xwayland_unmanaged_create(struct wlr_xwayland_surface *xsurface, bool mapped);
-
-void xwayland_view_create(struct wlr_xwayland_surface *xsurface, bool mapped);
 
 void xwayland_server_init(struct wlr_compositor *compositor);
 void xwayland_server_finish(void);
