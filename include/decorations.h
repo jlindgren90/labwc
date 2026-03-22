jlindgren90/labwc

commit 4f72e6775ea671cd07330a7a0b3039bd7d9e99aa

tree-wide: rename g_server to just server

include/decorations.h
@@ -7,12 +7,12 @@ struct view;
 struct wlr_surface;
 
 void kde_server_decoration_init(void);
-void xdg_server_decoration_init(void);
+void xdserver_decoration_init(void);
 
 void kde_server_decoration_update_default(void);
 void kde_server_decoration_set_view(struct view *view, struct wlr_surface *surface);
 
 void kde_server_decoration_finish(void);
-void xdg_server_decoration_finish(void);
+void xdserver_decoration_finish(void);
 
 #endif /* LABWC_DECORATIONS_H */

src/decorations/xdg-deco.c
@@ -115,22 +115,22 @@ xdg_toplevel_decoration(struct wl_listener *listener, void *data)
 }
 
 void
-xdg_server_decoration_init(void)
+xdserver_decoration_init(void)
 {
 	struct wlr_xdg_decoration_manager_v1 *xdg_deco_mgr = NULL;
-	xdg_deco_mgr = wlr_xdg_decoration_manager_v1_create(g_server.wl_display);
+	xdg_deco_mgr = wlr_xdg_decoration_manager_v1_create(server.wl_display);
 	if (!xdg_deco_mgr) {
 		wlr_log(WLR_ERROR, "unable to create the XDG deco manager");
 		exit(EXIT_FAILURE);
 	}
 
 	wl_signal_add(&xdg_deco_mgr->events.new_toplevel_decoration,
-		&g_server.xdg_toplevel_decoration);
-	g_server.xdg_toplevel_decoration.notify = xdg_toplevel_decoration;
+		&server.xdg_toplevel_decoration);
+	server.xdg_toplevel_decoration.notify = xdg_toplevel_decoration;
 }
 
 void
-xdg_server_decoration_finish(void)
+xdserver_decoration_finish(void)
 {
-	wl_list_remove(&g_server.xdg_toplevel_decoration.link);
+	wl_list_remove(&server.xdg_toplevel_decoration.link);
 }

commit 9392c066c86240bff93b5f4835c7a5209389e163

decorations: fix accidental rename
Fixes: 4f72e6775ea671cd07330a7a0b3039bd7d9e99aa
("tree-wide: rename g_server to just server")

include/decorations.h
@@ -7,12 +7,12 @@ struct view;
 struct wlr_surface;
 
 void kde_server_decoration_init(void);
-void xdserver_decoration_init(void);
+void xdg_server_decoration_init(void);
 
 void kde_server_decoration_update_default(void);
 void kde_server_decoration_set_view(struct view *view, struct wlr_surface *surface);
 
 void kde_server_decoration_finish(void);
-void xdserver_decoration_finish(void);
+void xdg_server_decoration_finish(void);
 
 #endif /* LABWC_DECORATIONS_H */

src/decorations/xdg-deco.c
@@ -115,7 +115,7 @@ xdg_toplevel_decoration(struct wl_listener *listener, void *data)
 }
 
 void
-xdserver_decoration_init(void)
+xdg_server_decoration_init(void)
 {
 	struct wlr_xdg_decoration_manager_v1 *xdg_deco_mgr = NULL;
 	xdg_deco_mgr = wlr_xdg_decoration_manager_v1_create(server.wl_display);
@@ -130,7 +130,7 @@ xdserver_decoration_init(void)
 }
 
 void
-xdserver_decoration_finish(void)
+xdg_server_decoration_finish(void)
 {
 	wl_list_remove(&server.xdg_toplevel_decoration.link);
 }

src/server.c
@@ -635,7 +635,7 @@ server_init(void)
 	seat_init();
 	xdg_shell_init();
 	kde_server_decoration_init();
-	xdserver_decoration_init();
+	xdg_server_decoration_init();
 
 	struct wlr_presentation *presentation = wlr_presentation_create(
 		server.wl_display, server.backend,
@@ -775,7 +775,7 @@ server_finish(void)
 	xdg_shell_finish();
 	layers_finish();
 	kde_server_decoration_finish();
-	xdserver_decoration_finish();
+	xdg_server_decoration_finish();
 	wl_list_remove(&server.new_constraint.link);
 	wl_list_remove(&server.output_power_manager_set_mode.link);
 	wl_list_remove(&server.tearing_new_object.link);